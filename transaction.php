b0d593b609f2aa5d7e2ec7046467ad57c89fe8b2
Amrit <amritpal1048@gmail.com>
2016-04-08 14:23:51 -0700

    testing

--- transaction.php
+++ transaction.php
@@ -1,8 +1,7 @@
 <?php
-<<<<<<< HEAD
+
 session_start();
-=======
->>>>>>> 0337edf3364d2851595cf4046db8dd3f6d1ac30d
+
 //Get promotion id
 //$pcode = $_SESSION['pcode'];
 // Get total
@@ -14,10 +13,6 @@ session_start();
  //$sqlt = "INSERT INTO Checkout (final_total, date_of_purchase) VALUES(".$_SESSION['total'].", DATE() )";
  $sql = "INSERT INTO Orders(user_id, total_amount, date_of_purchase) VALUES (".$_SESSION['uid'].",".$_SESSION['total'].", NOW())";
  $result = mysqli_query($con, $sql);
-<<<<<<< HEAD
-=======
- 
->>>>>>> 0337edf3364d2851595cf4046db8dd3f6d1ac30d
  
 header("Location: OrderMade.php"); 
 ?>
