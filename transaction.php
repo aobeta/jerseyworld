3a3ba3704b9cf64d6040ba818e6da847d5d134b6
Amrit <amritpal1048@gmail.com>
2016-04-08 14:11:16 -0700

    fixed transaction query

--- transaction.php
+++ transaction.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 //Get promotion id
 //$pcode = $_SESSION['pcode'];
 // Get total
@@ -8,10 +8,9 @@
 //$order = $_SESSION("orderId");
     
  $con = mysqli_connect("159.203.28.124", "Jersey", "letmein", "testing");
- $sqlt = "INSERT INTO Checkout (final_total, date_of_purchase) VALUES(".$_SESSION['total'].", DATE() )";
+ //$sqlt = "INSERT INTO Checkout (final_total, date_of_purchase) VALUES(".$_SESSION['total'].", DATE() )";
+ $sql = "INSERT INTO Orders(user_id, total_amount, date_of_purchase) VALUES (".$_SESSION['uid'].",".$_SESSION['total'].", NOW())";
  $result = mysqli_query($con, $sql);
-
- 
  
 header("Location: OrderMade.php"); 
 
